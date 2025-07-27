54d0b74d4d7a6883e079bc930d2669f0fe888e93
Krunal022 <krunalvaghela723@gmail.com>
2025-07-27 12:31:04 +0530

    JWT authentication and cookie!

--- TEN/src/routes/auth.routes.js
+++ TEN/src/routes/auth.routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const userModel = require('../models/user.model')
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const router = express.Router();
 
 express.json();
@@ -12,9 +14,16 @@ router.post('/register', async (req, res) => {
         password: password
     })
 
+    const token = jwt.sign({
+        id: user._id
+    }, process.env.JWT_SECRET)
+
+    res.cookie("token", token)
+
     res.status(201).json({
         message: "user is created",
-        user
+        user,
+        token
     })
 })
 
@@ -44,5 +53,27 @@ router.post('/login', async (req, res) => {
     })
 })
 
+
+router.get('/user', async (req, res) => {
+    const { token } = req.cookies;
+
+    if (!token) {
+        return res.status(401).json({
+            message: "Unauthorized!"
+        })
+    }
+
+    const decode = jwt.verify(token, process.env.JWT_SECRET)
+
+    const user = await userModel.findOne({
+        id: decode._id
+    }).select('-password -__v')
+
+    res.status(200).json({
+        message: "user fetched successfully",
+        user
+    })
+
+})
 
 module.exports = router
