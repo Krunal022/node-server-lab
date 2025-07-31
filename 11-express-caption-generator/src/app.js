444c056ff8964529d50f233c2b1a6246350087cb
Krunal022 <krunalvaghela723@gmail.com>
2025-07-31 23:33:28 +0530

    add GenAI - multer

--- 11-express-caption-generator/src/app.js
+++ 11-express-caption-generator/src/app.js
@@ -3,9 +3,11 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 const authRoutes = require('./routes/auth.routes');
+const postRoutes = require('./routes/post.routes')
 
 app.use(express.json())
 app.use(cookieParser())
 app.use('/api/auth', authRoutes);
+app.use('/api/posts', postRoutes)
 
 module.exports = app
---
+++ 11-express-caption-generator/src/services/ai.service.js
@@ -0,0 +1,17 @@
+const { GoogleGenAI } = require("@google/genai");
+require('dotenv').config()
+
+
+const ai = new GoogleGenAI({
+    apiKey: process.env.GEMINI_API_KEY
+});
+
+async function main() {
+    const response = await ai.models.generateContent({
+        model: "gemini-2.5-flash",
+        contents: "Explain how AI works in a few words",
+    });
+    console.log(response.text);
+}
+
+main();
