d02df0d24c4df14eea5a28193504f9f61885d7f7
Krunal022 <krunalvaghela723@gmail.com>
2025-08-08 23:31:12 +0530

    implement AI response!

--- 12-ai-chatbot-websoket/server.js
+++ 12-ai-chatbot-websoket/server.js
@@ -1,5 +1,5 @@
 const app = require('./src/app')
-
+const generateResponse = require('./src/services/ai.service')
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 
@@ -7,7 +7,24 @@ const httpServer = createServer(app);
 const io = new Server(httpServer, { /* options */ });
 
 io.on("connection", (socket) => {
-    // ...
+    console.log("user connected!")
+
+    socket.on("disconnect", () => {
+        console.log("user disconnected!")
+    })
+
+    socket.on("message", async (data) => {
+
+        console.log("AI Received :", data.prompt)
+        const response = await generateResponse(data.prompt);
+
+        console.log("AI Says :", response)
+
+        // Emit the response back to the client
+        // What AI Says!
+        socket.emit("ai-response", { response })
+    })
+
 });
 
 httpServer.listen(3000, () => {
