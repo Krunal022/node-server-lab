9cee68ceff5c4f81b54182a7f594bec00669f8a4
Krunal022 <krunalvaghela723@gmail.com>
2025-08-09 21:12:32 +0530

    EJS view engine support!

--- 13-ai-bot-gpt/src/app.js
+++ 13-ai-bot-gpt/src/app.js
@@ -1,5 +1,10 @@
 const express = require('express')
-
+const indexRoutes = require('./routes/index.route')
 const app = express()
+
+// set view engine for ejs templates
+app.set("view engine", 'ejs');
+app.use(express.static('public'));
+app.use('/', indexRoutes);
 
 module.exports = app;
