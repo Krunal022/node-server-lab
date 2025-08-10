9d4f2acbe02914d84303f6dbe8eb34d78d0db023
Krunal022 <krunalvaghela723@gmail.com>
2025-08-10 22:31:32 +0530

    registration  with JWT and cookie!

--- 13-ai-bot-gpt/src/app.js
+++ 13-ai-bot-gpt/src/app.js
@@ -5,13 +5,14 @@ const indexRoutes = require('./routes/index.route')
 const authRoutes = require('./routes/auth.route')
 
 const app = express()
-
+const cookieParser = require('cookie-parser');
 // set view engine for ejs templates
 app.set("view engine", 'ejs');
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(cookieParser());
 
 app.use('/', indexRoutes);
 app.use('/auth', authRoutes);
--- 13-ai-bot-gpt/src/controllers/auth.controller.js
+++ 13-ai-bot-gpt/src/controllers/auth.controller.js
@@ -1,15 +1,40 @@
+const { default: mongoose } = require('mongoose');
+const userModel = require('../models/user.model')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 async function getRegisterController(req, res) {
     res.render('register')
 }
 
-
 async function postRegisterController(req, res) {
-    console.log(req.body);
-    const user = req.body;
-    res.json({
-        message: "user resiger!",
-        user
+
+    const { username, email, password } = req.body;
+
+    const isUserExist = await userModel.findOne({
+        $or: [
+            { username: username },
+            { email: email }
+        ]
     })
+
+    if (isUserExist) {
+        return res.status(400).json({ message: 'User already exists' });
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await userModel.create({
+        username,
+        email,
+        password: hashPassword
+    });
+
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+
+    res.cookie('token', token);
+
+    res.status(201).json({ message: 'User registered successfully!', user: newUser, token });
 }
 
 async function getLoginController(req, res) {
