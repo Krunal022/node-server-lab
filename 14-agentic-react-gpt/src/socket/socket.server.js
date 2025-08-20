4694787b0b1814cd44f0e1fdba67bdb4bee55ad0
Krunal022 <krunalvaghela723@gmail.com>
2025-08-20 19:16:19 +0530

    auth check on socket connection!

--- 14-agentic-react-gpt/src/socket/socket.server.js
+++ 14-agentic-react-gpt/src/socket/socket.server.js
@@ -1,10 +1,36 @@
 const { Server } = require("socket.io");
+const cookie = require("cookie");
+const jwt = require('jsonwebtoken');
+const userModel = require('../models/user.model');
 
 async function initSocketServer(httpServer) {
     const io = new Server(httpServer, { /* options */ });
 
+    io.use(async (socket, next) => {
+
+        const cookies = cookie.parse(socket.handshake.headers?.cookie || "")
+
+        if (!cookies.token) {
+            next(new Error("Authentication error: No token provided!"));
+        }
+
+        try {
+            const decoded = jwt.verify(cookies.token, process.env.JWT_SECRET);
+
+            const user = await userModel.findById(decoded.id);
+
+            socket.user = user
+
+            next()
+
+        } catch (error) {
+            next(new Error("Authentication error: Invalid token!"))
+        }
+    })
+
     io.on("connection", (socket) => {
-        console.log("user connected!", socket.id);
+        console.log("User :", socket.user)
+        console.log("New socket Connection! :", socket.id);
     });
 }
 
